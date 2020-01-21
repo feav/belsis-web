41cad534aa405a2fa1bc818e99147976cc761b50
alex <azebaze@ausiteodit.com>
2020-01-21 07:40:16 +0100

    api

--- src/Controller/Api/CategorieController.php
+++ src/Controller/Api/CategorieController.php
@@ -55,9 +55,9 @@ class CategorieController extends APIController
 
         foreach ($categories as $key => $value) {
             if($value->getImage())
-                $image = $request->server->get('DOCUMENT_ROOT')."/images/uploads/categorie/".$value->getImage();
+                $image = $request->server->get('DOCUMENT_ROOT')."images/uploads/categorie/".$value->getImage();
             else
-                $image = $request->server->get('DOCUMENT_ROOT')."/images/image-default.jpeg";
+                $image = $request->server->get('DOCUMENT_ROOT')."images/image-default.jpeg";
 
             $categoriesArray[] = [
                 'id'=>$value->getId(),
