910b3a2c9421bdc5e341ae01e4e5ca08f0fd6b88
alex <azebaze@ausiteodit.com>
2020-01-28 19:34:17 +0100

    api

--- src/Controller/Api/CommandeController.php
+++ src/Controller/Api/CommandeController.php
@@ -352,7 +352,9 @@ class CommandeController extends APIController
         $commande->setEtat($request->get('etat'));
         if($request->get('etat') == "prete")
             $commande->setCuisinier($user->getId());
-        $msg = "La commande No:#$commande->getId() a été passé à $request->get('etat')"; 
+        $idCommande = $commande->getId();
+        $newEtat = $request->get('etat');
+        $msg = "La commande No:#$idCommande a été passé à $newEtat"; 
         $response =  $this->pushNotification($commande->getUser(), "Commande: changement d'etat", $msg);
 
         $entityManager->flush();
